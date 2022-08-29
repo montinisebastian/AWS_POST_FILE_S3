6fd065c9280d24a7b6523d0bc368a6ade52339d4
Montini Sebastian <59128891+montinisebastian@users.noreply.github.com>
2022-08-29 14:50:47 -0300

    OCR Lambda
    Capture Text of the PDF previously Uploaded as a user and application of First Version of the capture of the DNI in the Text through REGEX

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Cabecera from './components/Cabecera';
+import Upload from './App';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Cabecera />
+    <Upload />
   </React.StrictMode>,
   document.getElementById('root')
 );
