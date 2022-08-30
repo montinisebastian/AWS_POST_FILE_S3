442f40efd81b33bb46c12383affbcad157e7d8c7
Montini Sebastian <59128891+montinisebastian@users.noreply.github.com>
2022-08-30 03:16:27 -0300

    Add Buttons Styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
-import { useState } from 'react'
+import React, { Fragment, useState , useRef, useEffect} from "react";
 import axios from 'axios'
-import {Button,Form,FormGroup,Label,FormText,Input} from "reactstrap";
+import {Form,FormGroup,Label,FormText,Input} from "reactstrap";
 import './App.css'
-
+import { FileUpload } from 'primereact/fileupload';
+import { Toast } from 'primereact/toast';
+import { Button } from 'primereact/button';
+import { Divider } from 'primereact/divider';
 async function postImage({image, description}) {
   const formData = new FormData();
   formData.append("image", image)
@@ -21,7 +24,7 @@ function Upload() {
   const [fileName, setFileName] = useState()
   const [texto, setTexto] = useState("")
 
-  const submit = async event => {
+  const CargarPDF = async event => {
     event.preventDefault()
     const result = await postImage({image: file, description})
     setImages([result.image, ...images])
@@ -49,18 +52,41 @@ function Upload() {
     setFileName(event.target.files[0].name)
     console.log(event.target.files[0].name)
 	}
-  
+  const [totalSize, setTotalSize] = useState(0);
+    const toast = useRef(null);
+    const fileUploadRef = useRef(null);
+    const onUpload = () => {
+        toast.current.show({severity: 'info', summary: 'Success', detail: 'File Uploaded'});
+    }
   
   
 
   return (
-    <div className="App">
-      <form onSubmit={submit}>
-        <input onChange={fileSelected} type="file" accept="image/*"></input>
-        
-        <Button type="submit">Cargar</Button>
-        <Label>{texto}</Label>
-      </form>
+    <div class="md:p-6 align-items-center ">
+      <Divider align="center">
+        <div className="inline-flex align-items-center">
+          <i className="pi pi-map-marker mr-2"></i>
+          <b>Cargar Contrato</b>
+          </div>
+      </Divider>
+      <div  class="md:p-6 align-items-center "  >
+            <input onChange={fileSelected} type="file" accept="image/*"></input>
+                <div className="card">
+                      
+                        <FileUpload mode="basic" cancelOptions="display: none"  skinSimple="true"  cancelLabel="Cancelar" chooseLabel="Seleccionar Archivo" uploadLabel="." onBeforeSelect={fileSelected}  />
+                        
+                </div>
+              <Button  type="button" className="mr-3 p-button-raised" onClick={CargarPDF}>Cargar</Button>
+            <Label>{texto}</Label>
+        </div>  
+       
+      <Divider align="center">
+                    <div className="inline-flex align-items-center">
+                        <i className="pi pi-map-marker mr-2"></i>
+                        <b>Lugares Declarados</b>
+                    </div>
+      </Divider>
+      
 
       { images.map( image => (
         <div key={image}>
--- frontend/src/components/Cabecera.jsx
+++ frontend/src/components/Cabecera.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment, useState , useRef, useEffect} from "react";
 import { Button } from 'primereact/button';
+
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.css';
 import 'primeflex/primeflex.css'
@@ -7,7 +8,7 @@ import 'primeflex/primeflex.css'
 
 
 function Cabecera(){
-
+    
     return(
         <div  className="grid grid-nogutter surface-0 text-800">
             
@@ -29,7 +30,11 @@ function Cabecera(){
             <div className="col-12 md:col-6 overflow-hidden">
                 <img src={require(`../imagenes/sellos.jpg`).default} alt="hero-1" className="md:ml-auto block md:h-full"   style={{ clipPath: 'polygon(8% 0, 100% 0%, 100% 100%, 0 100%)' }} />
             </div>
+
+            
         </div>
+
+        
     )
 }
 
